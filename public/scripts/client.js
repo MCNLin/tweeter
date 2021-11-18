09a8659ce7ae3931d4b154be58004f00099ade95
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-18 02:13:50 +0000

    add built in alert function for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,64 +33,78 @@
 // ]
 //loops through the data and adds userinput to tweet
 function renderTweets(tweets) {
+  $('#tweet-container').empty();
   for (let tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $('#tweet-container').append($tweet);
+    $('#tweet-container').prepend($tweet);//adding tweets at the beginning
   }
 }
 
-const loadTweets = function(){ 
-  $.ajax('/tweets', { method: 'GET' })
-    .then(function (tweets) {
-      renderTweets(tweets);
-      console.log('Success: ', tweets)
-    });
-};
-loadTweets()
-// generate generic tweetelement outline
+
+// generate generic tweet element outline
 function createTweetElement (tweetData) {
   const {user, content, created_at} = tweetData;
   //create variable for outline of new tweets
   const tweetElementHTML = $(`<article class="tweet"> 
   <header>
-    <span class="username"><img src="${user.avatars}">${user.name}</span>
-    <span class="userHandle">${user.handle}</span>
+  <span class="username"><img src="${user.avatars}">${user.name}</span>
+  <span class="userHandle">${user.handle}</span>
     </header>
     <div class="tweet-content">
-      <p>${content.text}</p>
+    <p>${content.text}</p>
     </div>
     <footer>
-      <p>${timeago.format(created_at)}</p>
-      <div>
-        <i class="far fa-flag"></i> 
+    <p>${timeago.format(created_at)}</p>
+    <div>
+    <i class="far fa-flag"></i> 
         <i class="fas fa-retweet"></i>
         <i class="far fa-heart"></i>
-      </div>
+        </div>
     </footer>
-</article>`)
-  return tweetElementHTML;
+    </article>`)
+    return tweetElementHTML;
+};
+  //fetching tweets from page
+const loadTweets = function(){ 
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function (tweets) {
+        renderTweets(tweets);
+        console.log('Success: ', tweets)
+      });
 };
 
+const $form = $('form');
+    $form.on('submit', function (event) {
+      event.preventDefault()
+      console.log(event)
+      if (event.target[0].value === '') {
+        return alert('This message is empty');
+      }
+      if(event.target[0].value.length > 140) {
+        console.log("value---------->",event.target[0].length)
+        return alert('You have got a lot to say, remember only 140 characters!');
+      }
+      
+    });
 
 $(document).ready(function() {
-
 
   $("#post-tweet").submit(function(event){
     event.preventDefault();
     console.log("New Tweet Posted!");
-    // console.log("CurrentTarget---->",event.currentTarget.value)
-    console.log("target---->", event.target[0].value)
+    //figuring out where value is coming form
+    // console.log("target---->", event.target[0].value)
 
     $.ajax("/tweets",{
       method: "post",
-      data: $("#post-tweet").serialize()
+      data: $("#post-tweet").serialize(),
+      success: () => {loadTweets()}
     })
   
   });
 
-
+  loadTweets();
 });
 
-  // $("#tweet-container").prepend($tweet);
 
 
