0fc23c4ad8974b51e9a397da340bc04a8b793b9c
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-18 03:28:02 +0000

    add escape function to prevent XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,34 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-// const tweets = require("../../server/routes/tweets");
-
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 //loops through the data and adds userinput to tweet
 function renderTweets(tweets) {
   $('#tweet-container').empty();
