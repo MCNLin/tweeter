f67d7c25f7af6315ea1b1fd07f796596cbb4083d
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-19 00:24:58 +0000

    eslint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $('#tweet-text').on("input",function() {
+  $('#tweet-text').on("input", function() {
     let characterCount = $(this).val().length;
     let maxCharacters = 140;
     let remainingCharacter = maxCharacters - characterCount;
     // console.log(remainingCharacter)
     $(".counter").text(remainingCharacter);
-    //the counter becomes red when over the char limit   
+    //the counter becomes red when over the char limit
     if (remainingCharacter < 0) {
-      $(".counter").css("color","red");
+      $(".counter").css("color", "red");
     } else {
-      $(".counter").css("color","white");
+      $(".counter").css("color", "white");
     }
   });
-  
+
 });
